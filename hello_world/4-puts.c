ef8c4a4bc6616a063e6f4eb61773b99874bf820c
root <djay.mercado@gmail.com>
2024-02-19 13:58:24 +0000

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - puts - Programming is like building a multilingual puzzle
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    return (0);
+puts("\"Programming is like building a multilingual puzzle")
+return (0);
 }
