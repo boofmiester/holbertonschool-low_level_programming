8a9a086e8b3e95b70df1a55fda4d5130839d921f
root <djay.mercado@gmail.com>
2024-02-23 14:40:56 +0000

    federal agent Jack Bauer

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,13 +9,16 @@
 void jack_bauer(void)
 {
 int hour, minute;
-for (hour = 0; minute <= 59; minute++)
+for (hour = 0 ; hour <= 24 ; ++)
 {
-_putchar ((hour / 10) + '0');
+for (minute = 0 ; minute <= 59 ; minute++)
+{
+_Putchar ((hour / 10) + '0');
 _putchar ((hour % 10) + '0');
 _putchar (':');
 _putchar ((minute / 10) + '0');
 _putchar ((minute % 10) + '0');
 _putchar('\n');
+}
 }
 }
