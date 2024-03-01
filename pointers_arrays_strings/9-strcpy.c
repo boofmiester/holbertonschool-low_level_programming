867db0d713ced85787126fe53f23df90b7672714
root <djay.mercado@gmail.com>
2024-03-01 13:47:44 +0000

    strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * *_strcpy(char *dest, char *src) - function that copies the string
+ * *_strcpy (char *dest, char *src) - function that copies the string
  *                                   pointed to by src,
  *                                   including the terminating null byte (\0),
  *                                   to the buffer pointed to by dest.
