85a8823d3b5792c167d149f853c0a1db913097d5
root <djay.mercado@gmail.com>
2024-03-01 14:26:44 +0000

    Winning is only half of it. Having fun is the other half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -3,9 +3,6 @@
  * puts_half - prints half of a string,
  *             followed by a new line.
  * @str: char array string
- * Description: if the lenght of the string is odd, it prints the
- *              2nd half.
- *              if the lenght is even, it prints the last half.
  */
 
 void puts_half(char *str)
