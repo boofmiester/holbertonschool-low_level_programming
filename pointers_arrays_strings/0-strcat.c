21bc97027ff72540526c72150f49e34412e59c6d
root <djay.mercado@gmail.com>
2024-03-02 12:55:41 +0000

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -8,10 +8,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int index = 0, dest_len = 0;
-while (dest[index++])
-dest_len++;
-for (index = 0; src[index]; index++)
-dest[dest_len++] = src[index];
+int i, c;
+for (i = 0; dest[i] != '\0'; i++)
+;
+for (c = 0; src[c] != '\0'; c++)
+{
+dest[i] = src[c];
+i++;
+}
+dest[i] = '\0';
 return (dest);
 }
