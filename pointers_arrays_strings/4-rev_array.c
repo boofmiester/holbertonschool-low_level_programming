3ae802bd1436200f416dfadeb1a2c20d3af2ac33
root <djay.mercado@gmail.com>
2024-03-03 20:10:44 +0000

    I am a kind of paranoid in reverse

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -8,15 +8,12 @@
  */
 void reverse_array(int *a, int n)
 {
-int i, last;
-last = n - 1;
-for (i = 0; i < n / 2; i++)
+int i;
+int h;
+for (i = 0; i < (n / 2); i++)
 {
-int start, end;
-start = a[i];
-end = a[last];
-a[i] = end;
-a[last] = start;
-last--;
+h = a[i];
+a[i] = a[n - i - 1];
+a[n - i - 1] = h;
 }
 }
